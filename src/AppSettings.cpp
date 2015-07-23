bddbe7d5ab9fb41864e7d981bf69f81c3afbc91d
Dmitry <dmitry@pixoutserver.com>
2015-07-24 00:16:49 +0300

    Improved Settings

--- src/AppSettings.cpp
+++ src/AppSettings.cpp
@@ -2,35 +2,46 @@
 #include "Common/Logger.hpp"
 #include <QUrl>
 #include <QFile>
+#include <QApplication>
+
+#include <QtQml>
 
 AppSettings::AppSettings(QObject *parent) : QObject(parent),
-    port_(6454),position_( Vertical )
+    port_(6454),position_( Vertical ),app_path_( QString("%1").arg(QCoreApplication::applicationDirPath()) )
 {
-    fixture_path_ = "test";
+
 }
 
 AppSettings::~AppSettings()
 {
-
 }
 
-bool AppSettings::Load( const QString &path )
+//void AppSettings::declareQML()
+//{
+//        qmlRegisterType<Position>("PositionEnums", 1, 0, "Style");
+//}
+
+bool AppSettings::load( const QString &path )
 {
-    settings_path_ = path;
+    settings_path_ = path ;
 
     QFile file(settings_path_);
-    if( !file.open(QIODevice::ReadOnly))
-        return true;    // file could not be created
+    if( !file.open(QIODevice::ReadWrite))
+        return false;    // file could not be created
 
     if( !file.size() )
-        return true;
+        return false;
 
     LOG( 1, "Loading settings from %s ", qPrintable( settings_path_ ) );
+
 
     QDataStream ds(&file);
     ds >> port_;
     ds >> fixture_path_;
     ds >> position_int_;
+
+    emit fixturePathChanged();
+    emit onLoaded();
 
     return true;
 }
@@ -54,4 +65,11 @@ bool AppSettings::Save( const QString &path )
 
     return true;
 
+}
+
+QString AppSettings::stripURLFromPath( const QString &path )
+{
+    QUrl url(path);
+
+    return url.isLocalFile() ? url.toLocalFile() : path;
 }
--- src/AppSettings.hpp
+++ src/AppSettings.hpp
@@ -2,31 +2,38 @@
 #define APPSETTINGS_H
 
 #include <QObject>
+#include <QUrl>
 
 class AppSettings : public QObject
 {
     Q_OBJECT
-    Q_ENUMS(Position)
     Q_PROPERTY(int port MEMBER port_ NOTIFY portChanged)
     Q_PROPERTY(QString fixturePath MEMBER fixture_path_ NOTIFY fixturePathChanged)
     Q_PROPERTY(Position position MEMBER position_ NOTIFY positionChanged)
+    Q_PROPERTY(QUrl appPath MEMBER app_path_ CONSTANT)
 
 public:
     explicit AppSettings(QObject *parent = 0);
     ~AppSettings();
 
-     bool Load( const QString &path );
+    Q_INVOKABLE QString stripURLFromPath( const QString &path );
+    Q_INVOKABLE bool load( const QString &path );
      bool Save( const QString &path );
      enum Position { Vertical, Horizontal };
+    Q_ENUMS(Position)
 
      int port() const { return port_; }
      const QString& fixturePath() const { return fixture_path_; }
+     QString& fixturePath() { return fixture_path_; }
      Position position() const { return position_; }
+
+//     static void declareQML();
 
 signals:
       void portChanged();
       void fixturePathChanged();
       void positionChanged();
+      void onLoaded();
 
 private:
       int port_;
@@ -38,6 +45,7 @@ private:
       };
 
       QString settings_path_;
+      QUrl app_path_;
 
 };
 
